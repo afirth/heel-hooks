bac974310334c6bb7764c6c44749db8e1a79d331
afirth <afirth@users.noreply.github.com>
2018-11-30 12:42:37 +0200

    fix path of static files

--- heel-hooks.go
+++ heel-hooks.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("/static/chris_heelhook.jpg"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/static"))))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
